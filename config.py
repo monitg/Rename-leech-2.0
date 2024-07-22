6adc426c51247d6b3a831a8f99534414a9a64e24
monitg <142651228+monitg@users.noreply.github.com>
2024-07-22 10:48:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,20 +6,20 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
-CAPTION = os.environ.get("CAPTION", "")
-group = environ.get('GROUP', '-1002128043143')
+API_ID = os.environ.get("API_ID", "16229284")
+API_HASH = os.environ.get("API_HASH", "ebd1fead3cc15343bea10b5c164165ba")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7148650828:AAHbynnhmG6gymtPYAYf_jyinDZh1R6stFQ")
+ADMIN = int(os.environ.get("ADMIN", '1103137195'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Team_MDL")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "0")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://monitgmoni:MoniTG007@cluster0.bxwiejd.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "RenameLeechbot")
+CAPTION = os.environ.get("CAPTION", "<b>{filename}/n/n🥀 Powered By : @Team_MDL</b>")
+group = environ.get('GROUP', '-1002208828378')
 GROUP = int(group) if group and id_pattern.search(group) else None
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+SUNRISES_PIC= "https://telegra.ph/file/9acca333738f41eba89f8.jpg"  # Replace with your Telegraph link
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '1103137195'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
 LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002100717987)
